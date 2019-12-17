56e2180ff0bf30def11d58d5243183152e8e021f
SalvatoreArdimento <salvatoreardimento.lav@gmail.com>
2019-12-17 09:31:50 +0100

    checkcard/

---
+++ checkcard/src/checkdigit.java
@@ -0,0 +1,12 @@
+public class checkdigit {
+    public static void main(String[] args) {
+        CardNo we = new CardNo();
+        System.out.print(we.checkSum("01234567"));
+
+
+        //we.setEAN13();
+        //String[] i = we.getEAN13();
+        //we.checksum(i);
+    }
+
+}
