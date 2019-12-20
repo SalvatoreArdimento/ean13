e99b34d2a715f34c4bf893e8505a712180b117fe
SalvatoreArdimento <salvatoreardimento.lav@gmail.com>
2019-12-20 12:02:39 +0100

    Aggiunta provaListe2

--- checkcard/src/provaListe2.java
+++ checkcard/src/provaListe2.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class provaListe {
+public class provaListe2 {
     public static void main(String[] args) throws IOException {
-        String s;
+        String s = "";
         int i;
         CardNumber cardNumber = new CardNumber("");
         File fileIn = new File("./in.txt");
@@ -13,19 +13,13 @@ public class provaListe {
         BufferedReader reader = new BufferedReader(new FileReader(fileIn));
         List<String> listIn = new ArrayList<String>();
         List<String> listOut = new ArrayList<String>();
-        do
-        {                                                            //Prendo i codici dal file e li inserisco nella lista listIn
-            s = reader.readLine();
-            if (s != null) {
-                listIn.add(s);
-
-            }
-        } while (s != null);
+        //Prendo i codici dal file e li inserisco nella lista listIn
+        while ((s = reader.readLine()) != null) listIn.add(s);
         reader.close();
         for (i = 0; i < listIn.size(); i++) {                        //Utilizzo l'oggetto 'cardnumber' usando i codici di listaIn e inserisco i codici completi in listOut
             cardNumber.setCardNo(listIn.get(i));
-            if(cardNumber.isLengthCorrect())
-            listOut.add(cardNumber.getFullCardNumber());
+            if (cardNumber.isLengthCorrect())
+                listOut.add(cardNumber.getFullCardNumber());
             //System.out.println(i);
         }
         for (i = 0; i < listOut.size(); i++) {                      //Stampo listOut su file
