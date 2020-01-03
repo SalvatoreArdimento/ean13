8c7c09e1be301cfe018e0612b63988430fb77a83
SalvatoreArdimento <salvatoreardimento.lav@gmail.com>
2020-01-03 17:15:07 +0100

    fix al metodo close

---
+++ checkcard/src/dbmanagement/Db.java
@@ -0,0 +1,46 @@
+package dbmanagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+    private final String url = "jdbc:postgresql://localhost:5432/checkcard";
+    private final String user = "postgres";
+    private final String password = "developer2";
+    private int status;
+    private String statusMsg;
+    private Connection conn = null;
+
+
+    public Connection connect() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                this.status = 0;
+            } catch (SQLException e) {
+                this.status = 1;
+            }
+        }
+        return conn;
+    }
+
+    public String getStatusMsg() {
+        return statusMsg;
+    }
+
+    public void close() throws SQLException {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        } else
+            System.out.println("nessuna connessione da chiudere");
+    }
+}
+
+
+
+
---
+++ checkcard/src/dbmanagement/provaDbManagement.java
@@ -0,0 +1,15 @@
+package dbmanagement; 
+
+import java.sql.SQLException;
+
+public class provaDbManagement {
+    public static void main (String [] args)throws SQLException {
+        Db db = new Db();
+        db.close();
+
+
+
+
+
+    }
+}
