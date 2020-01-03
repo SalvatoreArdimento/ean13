2e0be09f54953047684c3c57036ca62b02c721ca
SalvatoreArdimento <salvatoreardimento.lav@gmail.com>
2020-01-03 16:21:06 +0100

    aggiunta classe per il controllo di una connessione a database

--- checkcard/src/dbManagement/provaDbManagement.java
+++ checkcard/src/dbManagement/provaDbManagement.java
@@ -4,8 +4,7 @@ import java.sql.SQLException;
 
 public class provaDbManagement {
     public static void main (String [] args)throws SQLException {
-        Db db = new Db();
-        db.connect();
+        
         db.CloseConn();
 
 
